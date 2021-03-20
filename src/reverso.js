3296109215b9cf6076a864f79f5e2dda8979007a
s0ftik3 <slava021323@gmail.com>
2021-03-20 11:50:26 +0600

    Fix #5.

--- src/reverso.js
+++ src/reverso.js
@@ -1,15 +1,14 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const urls = require('./urls'); 
+const urls = require('./urls');
 const { 
     checkContextLang, 
     checkSpellLang, 
-    checkSynonymsLang,
+    checkSynonymsLang, 
     checkTranslationLang
 } = require('./langcheck');
 
-class Reverso {
-
+module.exports = class Reverso {
     /**
      * Retrieves information about target text as its translation, usage examples.
      * Tests have shown that the method's average latency is ~1.3sec.
@@ -27,46 +26,47 @@ class Reverso {
             method: 'GET',
             url: url,
             headers: {
-                Accept: 'application/json, text/javascript, */*; q=0.01',
+                Accept:
+                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 Connection: 'keep-alive',
-                'Content-Type': 'application/json; charset=UTF-8',
-                Origin: 'https://reverso.net',
-                'Sec-Fetch-Dest': 'empty',
-                'Sec-Fetch-Mode': 'cors',
-                'Sec-Fetch-Site': 'same-site'
-            }
-        }).then((response) => {
-            const $ = cheerio.load(response.data);
-            let examples = [];
-            let translation = [];
+                'User-Agent':
+                    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36',
+            },
+        })
+            .then((response) => {
+                const $ = cheerio.load(response.data);
+                let examples = [];
+                let translation = [];
 
-            let fromExample = $('body').find('.example').find('div[class="src ltr"] > span[class="text"]').text().trim().split('\n');
-            let toExample = $('body').find('.example').find('div[class="trg ltr"] > span[class="text"]').text().trim().split('\n');
-            let toTranslation = $('body').find('div[id="translations-content"]').text().split('\n');
+                let fromExample = $('body').find('.example').find('div[class="src ltr"] > span[class="text"]').text().trim().split('\n');
+                let toExample = $('body').find('.example').find('div[class="trg ltr"] > span[class="text"]').text().trim().split('\n');
+                let toTranslation = $('body').find('div[id="translations-content"]').text().split('\n');
 
-            for (let i = 0; i < fromExample.length; i++) {
-                examples.push({
-                    id: i,
-                    from: fromExample[i].trimStart(),
-                    to: toExample[i].trimStart()
+                for (let i = 0; i < fromExample.length; i++) {
+                    examples.push({
+                        id: i,
+                        from: fromExample[i].trimStart(),
+                        to: toExample[i].trimStart(),
+                    });
+                }
+
+                toTranslation.forEach((e) => {
+                    let string = e.trim();
+                    if (string.length <= 0) return;
+                    translation.push(e.trim());
                 });
-            }
 
-            toTranslation.forEach(e => {
-                let string = e.trim();
-                if (string.length <= 0) return;
-                translation.push(e.trim());
+                return {
+                    text: text,
+                    from: from,
+                    to: to,
+                    translation: translation.filter((e) => e != text),
+                    examples: examples,
+                };
+            })
+            .catch((err) => {
+                throw new Error('reverso.net did not respond or there are no context examples for the given text.\n' + err);
             });
-
-            return {
-                text: text,
-                from: from,
-                to: to,
-                translation: translation.filter(e => e != text),
-                examples: examples
-            };
-
-        }).catch((err) => { throw new Error('reverso.net did not respond or there are no context examples for the given text.\n' + err) });
     }
 
     /**
@@ -78,10 +78,10 @@ class Reverso {
      */
     getSpellCheck(text, lang) {
         checkSpellLang(lang);
-        
+
         let resLang = {
-            'english': 'eng',
-            'french': 'fra'
+            english: 'eng',
+            french: 'fra',
         };
 
         let url = urls.spellCheckUrl + `?text=${encodeURIComponent(text)}&language=${resLang[lang.toLowerCase()]}&getCorrectionDetails=true`;
@@ -90,31 +90,33 @@ class Reverso {
             method: 'GET',
             url: url,
             headers: {
-                Accept: 'application/json, text/javascript, */*; q=0.01',
+                Accept:
+                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 Connection: 'keep-alive',
-                'Content-Type': 'application/json; charset=UTF-8',
-                Origin: 'https://reverso.net',
-                'Sec-Fetch-Dest': 'empty',
-                'Sec-Fetch-Mode': 'cors',
-                'Sec-Fetch-Site': 'same-site'
-            }
-        }).then((response) => {
-            let data = response.data;
-            let result = [];
+                'User-Agent':
+                    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36',
+            },
+        })
+            .then((response) => {
+                let data = response.data;
+                let result = [];
 
-            for (let i = 0; i < data.corrections.length; i++) {
-                result.push({
-                    id: i,
-                    text: text,
-                    type: data.corrections[i].type,
-                    explanation: data.corrections[i].longDescription,
-                    corrected: data.corrections[i].correctionText,
-                    full_corrected: data.text,
-                });
-            }
+                for (let i = 0; i < data.corrections.length; i++) {
+                    result.push({
+                        id: i,
+                        text: text,
+                        type: data.corrections[i].type,
+                        explanation: data.corrections[i].longDescription,
+                        corrected: data.corrections[i].correctionText,
+                        full_corrected: data.text,
+                    });
+                }
 
-            return result;
-        }).catch((err) => { throw new Error('reverso.net did not respond or your text has no mistakes.\n' + err) });
+                return result;
+            })
+            .catch((err) => {
+                throw new Error('reverso.net did not respond or your text has no mistakes.\n' + err);
+            });
     }
 
     /**
@@ -128,13 +130,13 @@ class Reverso {
         checkSynonymsLang(lang);
 
         let resLang = {
-            'english': 'en',
-            'french': 'fr',
-            'german': 'de',
-            'russian': 'ru',
-            'italian': 'it',
-            'polish': 'pl',
-            'spanish': 'es'
+            english: 'en',
+            french: 'fr',
+            german: 'de',
+            russian: 'ru',
+            italian: 'it',
+            polish: 'pl',
+            spanish: 'es',
         };
 
         let url = urls.synonymsUrl + `${resLang[lang.toLowerCase()]}/${encodeURIComponent(text)}`;
@@ -143,32 +145,36 @@ class Reverso {
             method: 'GET',
             url: url,
             headers: {
-                Accept: 'application/json, text/javascript, */*; q=0.01',
+                Accept:
+                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 Connection: 'keep-alive',
-                'Content-Type': 'application/json; charset=UTF-8',
-                Origin: 'https://reverso.net',
-                'Sec-Fetch-Dest': 'empty',
-                'Sec-Fetch-Mode': 'cors',
-                'Sec-Fetch-Site': 'same-site'
-            }
-        }).then((response) => {
-            const $ = cheerio.load(response.data);
-            const synonyms = [];
+                'User-Agent':
+                    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36',
+            },
+        })
+            .then((response) => {
+                const $ = cheerio.load(response.data);
+                const synonyms = [];
 
-            // thanks to https://stackoverflow.com/questions/32655076/cheerio-jquery-selectors-how-to-get-a-list-of-elements-in-nested-divs
-            $('body').find(`a[class="synonym  relevant"]`).each((i, e) => {
-                synonyms.push({
-                    id: i,
-                    synonym: $(e).text()
-                });
+                // thanks to https://stackoverflow.com/questions/32655076/cheerio-jquery-selectors-how-to-get-a-list-of-elements-in-nested-divs
+                $('body')
+                    .find(`a[class="synonym  relevant"]`)
+                    .each((i, e) => {
+                        synonyms.push({
+                            id: i,
+                            synonym: $(e).text(),
+                        });
+                    });
+
+                return {
+                    text: text,
+                    from: lang,
+                    synonyms: synonyms,
+                };
+            })
+            .catch((err) => {
+                throw new Error('reverso.net did not respond or there are no synonyms for the given text.\n' + err);
             });
-
-            return {
-                text: text,
-                from: lang,
-                synonyms: synonyms
-            };
-        }).catch((err) => { throw new Error('reverso.net did not respond or there are no synonyms for the given text.\n' + err) });
     }
 
     /**
@@ -184,101 +190,93 @@ class Reverso {
         checkTranslationLang(from, to);
 
         let lang = {
-            'english': 'eng',
-            'french': 'fra',
-            'german': 'ger',
-            'russian': 'rus',
-            'italian': 'ita',
-            'polish': 'pol',
-            'spanish': 'spa'
+            english: 'eng',
+            french: 'fra',
+            german: 'ger',
+            russian: 'rus',
+            italian: 'ita',
+            polish: 'pol',
+            spanish: 'spa',
         };
 
         let voices = {
-            'english': 'Heather22k',
-            'french': 'Alice22k',
-            'german': 'Claudia22k',
-            'russian': 'Alyona22k',
-            'italian': 'Chiara22k',
-            'polish': 'Ania22k',
-            'spanish': 'Ines22k'
+            english: 'Heather22k',
+            french: 'Alice22k',
+            german: 'Claudia22k',
+            russian: 'Alyona22k',
+            italian: 'Chiara22k',
+            polish: 'Ania22k',
+            spanish: 'Ines22k',
         };
 
-        return axios.post(urls.translation, {
-            format: 'text',
-            from: lang[from.toLowerCase()],
-            input: text,
-            options: {
-                contextResults: true,
-                languageDetection: true,
-                origin: 'reversomobile',
-                sentenceSplitter: false
-            },
-            to: lang[to.toLowerCase()],
-            headers: {
-                Accept: 'application/json, text/javascript, */*; q=0.01',
-                Connection: 'keep-alive',
-                'Content-Type': 'application/json; charset=UTF-8',
-                Origin: 'https://reverso.net',
-                'Sec-Fetch-Dest': 'empty',
-                'Sec-Fetch-Mode': 'cors',
-                'Sec-Fetch-Site': 'same-site'
-            }
-        }).then(response => {
+        return axios
+            .post(urls.translation, {
+                format: 'text',
+                from: lang[from.toLowerCase()],
+                input: text,
+                options: {
+                    contextResults: true,
+                    languageDetection: true,
+                    origin: 'reversomobile',
+                    sentenceSplitter: false,
+                },
+                to: lang[to.toLowerCase()],
+                headers: {
+                    Accept:
+                        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                    Connection: 'keep-alive',
+                    'User-Agent':
+                        'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36',
+                },
+            })
+            .then((response) => {
+                let textToVoice = Buffer.from(response.data.translation[0]).toString('base64');
+                let condition = voices[to.toLowerCase()] != undefined && response.data.translation[0].length <= 150;
 
-            let textToVoice = Buffer.from(response.data.translation[0]).toString('base64');
-            let condition = voices[to.toLowerCase()] != undefined && response.data.translation[0].length <= 150;
+                let contextExamples = [];
 
-            let contextExamples = [];
+                if (response.data.contextResults == null || response.data.contextResults.results.length <= 0) {
+                    return {
+                        text: text,
+                        from: response.data.from,
+                        to: response.data.to,
+                        translation: response.data.translation,
+                        context: {
+                            examples: 'no context examples',
+                            rude: 'not defined',
+                        },
+                        detected_language: response.data.languageDetection.detectedLanguage,
+                        voice: condition ? `${urls.voice}voiceName=${voices[to.toLowerCase()]}?inputText=${textToVoice}` : false,
+                    };
+                } else {
+                    let sourceExamples = response.data.contextResults.results[0].sourceExamples;
+                    let targetExamples = response.data.contextResults.results[0].targetExamples;
 
-            if (response.data.contextResults == null || response.data.contextResults.results.length <= 0) {
+                    for (let i = 0; i < sourceExamples.length; i++) {
+                        contextExamples.push({
+                            from: sourceExamples[i].replace(/<[^>]*>/gi, ''),
+                            to: targetExamples[i].replace(/<[^>]*>/gi, ''),
+                            phrase_from: sourceExamples[i].match(/(?<=<em>)(.*?)(?=<\/em>)/gi)[0],
+                            phrase_to: targetExamples[i].match(/(?<=<em>)(.*?)(?=<\/em>)/gi)[0],
+                        });
+                    }
 
-                return {
-                    text: text,
-                    from: response.data.from,
-                    to: response.data.to,
-                    translation: response.data.translation,
-                    context: {
-                        examples: 'no context examples',
-                        rude: 'not defined'
-                    },
-                    detected_language: response.data.languageDetection.detectedLanguage,
-                    voice: (condition) ? `${urls.voice}voiceName=${voices[to.toLowerCase()]}?inputText=${textToVoice}` : false
-                };
-
-            } else {
-
-                let sourceExamples = response.data.contextResults.results[0].sourceExamples;
-                let targetExamples = response.data.contextResults.results[0].targetExamples;
-    
-                for (let i = 0; i < sourceExamples.length; i++) {
-    
-                    contextExamples.push({
-                        from: sourceExamples[i].replace(/<[^>]*>/gi, ''),
-                        to: targetExamples[i].replace(/<[^>]*>/gi, ''),
-                        phrase_from: sourceExamples[i].match(/(?<=<em>)(.*?)(?=<\/em>)/gi)[0],
-                        phrase_to: targetExamples[i].match(/(?<=<em>)(.*?)(?=<\/em>)/gi)[0],
-                    });
-                    
+                    return {
+                        text: text,
+                        from: response.data.from,
+                        to: response.data.to,
+                        translation: response.data.translation,
+                        context: {
+                            examples: contextExamples,
+                            rude: response.data.contextResults.results[0].rude,
+                        },
+                        detected_language: response.data.languageDetection.detectedLanguage,
+                        voice: condition ? `${urls.voice}voiceName=${voices[to.toLowerCase()]}?inputText=${textToVoice}` : false,
+                    };
                 }
-
-                return {
-                    text: text,
-                    from: response.data.from,
-                    to: response.data.to,
-                    translation: response.data.translation,
-                    context: {
-                        examples: contextExamples,
-                        rude: response.data.contextResults.results[0].rude
-                    },
-                    detected_language: response.data.languageDetection.detectedLanguage,
-                    voice: (condition) ? `${urls.voice}voiceName=${voices[to.toLowerCase()]}?inputText=${textToVoice}` : false
-                };
-
-            }
-
-        }).catch((err) => { throw new Error('reverso.net did not respond or there is no translation for the given text.\n' + err) });
+            })
+            .catch((err) => {
+                throw new Error('reverso.net did not respond or there is no translation for the given text.\n' + err);
+            });
     }
-
-}
-
-module.exports = Reverso;
+};
